2e0418cc0605d7b85123ae430d5b6f196f131b86
P. Henryck DEV <pedrohenryckoliveiragomes@gmail.com>
2025-03-28 08:30:32 -0300

    string

---
+++ 6-strings.py
@@ -0,0 +1,23 @@
+# Strings
+
+nomeFilme = 'top gun'
+nomeFilme2 = 'Top Gun'
+
+
+print(nomeFilme == nomeFilme2)# Vendo se mesmo sendo a mesma plavara mas uma minuscula e a outra maiuscula são iguais. 
+
+# Multi linhas
+descricaoFilme = """
+            Top gun é um filme de aviação e aventura
+            muito forte na indústria"""
+
+print(nomeFilme2)
+
+# Multiplicação de strings
+print('-'*40)
+
+print(descricaoFilme)
+
+# Procurar uma plaavra dentro de um texto
+print('Top' in nomeFilme2)
+print('ação' in nomeFilme)
