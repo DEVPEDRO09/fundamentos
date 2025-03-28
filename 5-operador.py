3198f3351d75a7ef1373603ece0dfa5196b83bd8
P. Henryck DEV <pedrohenryckoliveiragomes@gmail.com>
2025-03-28 01:15:28 -0300

    Operador

---
+++ 5-operador.py
@@ -0,0 +1,52 @@
+# Operadores 
+
+""" > maior que 
+    < menor que 
+    == igual 
+    != diferente 
+    >= maior iqual 
+    <= menor iqual
+
+    """
+""" + soma 
+    * multiplicaço 
+    / divisão
+    - subtraçao
+    % resto
+    ** Exponenciação
+    """
+
+num1 = int(input('Digite um numero:\n'))
+num2 = int(input('Digite outro numero:\n'))
+
+maior = num1 > num2 
+menor = num1 < num2 
+igual = num1 == num2
+diferente = num1 != num2 
+maiorIgual = num1 >= num2
+menorIgual = num1 <= num2
+
+
+
+soma = num1 + num2 
+multiplicacao = num1 * num2 
+divisao = num1 / num2
+subtracao = num1 - num2
+resto = num1 % num2
+exponenciacao = num1 ** num2
+
+print(maior)
+print(menor)
+print(igual)
+print(diferente)
+print(maiorIgual)
+print(menorIgual)
+
+print()
+
+print(soma)
+print(multiplicacao)
+print(divisao)
+print(subtracao)
+print(resto)
+print(exponenciacao)
