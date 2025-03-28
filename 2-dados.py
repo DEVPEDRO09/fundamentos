e1ead179d9b2e43a37f5de5d36f236c42258e346
P. Henryck DEV <pedrohenryckoliveiragomes@gmail.com>
2025-03-28 00:29:23 -0300

    Tipos de dados

---
+++ 2-dados.py
@@ -0,0 +1,18 @@
+# filme
+
+nome = 'Top Gun Maverick'
+anoLancamento = 2023
+notafilme = 9.5
+incluidoPlano = True
+
+print(nome)
+print(anoLancamento)
+print(notafilme)
+print(incluidoPlano)
+
+print('-'*60)
+
+print(type(nome))
+print(type(anoLancamento))
+print(type(notafilme))
+print(type(incluidoPlano))
