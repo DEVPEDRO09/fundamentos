8bb9d7026b12135e935a3622c095c0126d1c0083
P. Henryck DEV <pedrohenryckoliveiragomes@gmail.com>
2025-03-28 09:33:45 -0300

    Slice

---
+++ 7-slice.py
@@ -0,0 +1,23 @@
+filme = 'top gun'
+
+# string [inicio:fim] - indice começa n posição 0 / indice finl -1
+
+# 1 - buscar toda string partir da primeira posição 
+print(filme[0:])
+
+# 2 - buscar todas string ate a ultima posição
+print(filme[ :7])
+
+# 3 - buscar toda string da terceira até a ultima 
+print(filme [2: ])
+
+# Passo - determina o incremento por padrão esse numero é o 1.
+
+# 4 - buscar toda a string de 2 em 2 caracteres
+print(filme [: :2])
+
+# 5 - buscar toda string dos indices impares
+print(filme [1 : : 2])
+
+# 6 - inverter uma string de trás para frente 
+print(filme [: : -1])
