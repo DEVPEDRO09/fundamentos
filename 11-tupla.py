9df06fef74a9180cc12cbc9bfa72f5be9396d038
P. Henryck DEV <pedrohenryckoliveiragomes@gmail.com>
2025-03-28 11:52:07 -0300

    tupla

---
+++ 11-tupla.py
@@ -0,0 +1,18 @@
+tuplaFilmes = ('Velozes e Furiosos', 'Interestellar','Carros','Toys Story','Vingadores')
+
+print(type(tuplaFilmes))
+
+# 1 - Buscar os dois primeiros itens da tupla
+print(tuplaFilmes[:2])
+
+# 2 - Buscar o ultimo iten da tupla
+print(tuplaFilmes[-1])
+
+# 3 - Buscar filmes até uma determinada posição
+print(tuplaFilmes[:3])
+
+# 4 - Buscar um filme da posição em diante 
+print(tuplaFilmes[2:])
+
+# 5 - Recuperar um iten da tupla 
+print(tuplaFilmes.index('Vingadores'))
